9655c09d65d596061f64dd99759d11d7a004b456
AdamOlsonMN <akomonday@gmail.com>
2014-02-16 19:06:06 -0600

    I did a bunch of aggregating for the ideological measurements. They aren't pretty but I think it works. I also started writing an outline.

--- PrelimAnal.R
+++ PrelimAnal.R
@@ -3,6 +3,8 @@
 library(foreign)
 library(stargazer)
 library(ggplot2)
+library(plyr)
+library(reshape2)
 
 # Import Data
 NokkenPooleHouseScores <- read.csv("C:/Users/Olson/Dropbox/ideas/Tea Party Does Not Exist/NokkenPooleHouse.csv")
@@ -11,6 +13,9 @@ NokkenPooleHouseScores <- read.csv("C:/Users/Olson/Dropbox/ideas/Tea Party Does 
 dav110 <- NokkenPooleHouseScores[ which(NokkenPooleHouseScores$Congress >= 110), ]
 # Only Post War
 dav80 <- NokkenPooleHouseScores[ which(NokkenPooleHouseScores$Congress >= 80), ]
+# Only 20th Century
+dav56 <- NokkenPooleHouseScores[ which(NokkenPooleHouseScores$Congress >= 56), ]
+
 
 ### Part One
 ### Prelim Analysis
@@ -40,22 +45,48 @@ p1gv2 + geom_point(aes(colour = factor(Party)))
 ### trends historically. For this I have to figure out how to extract averages
 ### per congress in order to find the pivots. Eventually I'll also need Senate
 ### data.
-## Requires the Reshape package
-library(reshape)
-p2daAgg <-aggregate(dav80, by=list(dav80$Congress, dav80$Party), FUN=mean, na.rm=TRUE)
+### Additionally, I had the idea of looking at extremism over all Congresses to see 
+### if there has been a period where the parties were this extreme. Compare and 
+### contrast. My thought is that we can compare contemporary Republicans to TP
+### Republicans
 
-## For sake of temporary clenliness, drop non gophers or dems.
-## This is broken
-p2daAgg2 <- p2daAgg[ which(p2daAgg$party == 100 & p2daAgg$party == 200), ]
+## This is an extremely clumsy but working way of getting the average dw nominate
+## scores for a given party in a given congress. I do it for both the post war
+## and also the 20th century
+# Post War
+Agg80 <-aggregate(dav80$FirstDimension, by=list(dav80$Congress,dav80$Party), 
+                  FUN=mean, na.rm=TRUE)
+Agg80 <- rename(Agg80, c(Group.1="Congress"))
+Agg80 <- rename(Agg80, c(Group.2="Party"))
+Agg80 <- rename(Agg80, c(x="AverageIdeo"))
+
+Agg80Final <- dcast(Agg80, Congress ~ Party)
+Agg80Final <- rename(Agg80Final, c("100"="Democrat"))
+Agg80Final <- rename(Agg80Final, c("200"="Republican"))
+
+# 20th century
+Agg56 <-aggregate(dav56$FirstDimension, by=list(dav56$Congress,dav56$Party), 
+                  FUN=mean, na.rm=TRUE)
+Agg56 <- rename(Agg56, c(Group.1="Congress"))
+Agg56 <- rename(Agg56, c(Group.2="Party"))
+Agg56 <- rename(Agg56, c(x="AverageIdeo"))
+
+Agg56Final <- dcast(Agg56, Congress ~ Party)
+Agg56Final <- rename(Agg56Final, c("100"="Democrat"))
+Agg56Final <- rename(Agg56Final, c("200"="Republican"))
+
+
 
 ## Plots of Aggregate Data
 ## These pplots aren't working because the party shit is so diffused. 
 
 ## That does not work. Aggregate ideology scores not compiling. Need to drop Pres ahead of time. Let's plot it anyway.
 ## For some reason can't plot by party. Do not know why. 
-p2g1 <- ggplot(p2daAgg2, aes(p2daAgg2$Congress, p2daAgg2$FirstDimension))
+p2g1 <- ggplot(Agg56Final, aes(Agg56Final$Democrat, Agg56Final$Republican))
 p2g1 + geom_point()
 p2g1 + facet_grid(p2daAgg2$party)
 
 # Let's try using a regular line plot
 p2g2 <- ggplot(p2daAgg, aes((x=p2daAgg$Congress, y=p2daAgg$FirstDimension, color=color = as.factor(p2daAgg$party)))
+
+               
