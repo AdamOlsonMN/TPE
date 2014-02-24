1169ff98552e1782faf80a6f6800789baa524e88
AdamOlsonMN <akomonday@gmail.com>
2014-02-23 22:36:08 -0600

    I didn't do much in this edit. Still reading about conservative history to develop theory. Basically we see a change in conservatives over the last fourty years where they become willing to use government levels to accomplish their ideological goals. In any case, added some code dealing with the data handling of parties. Should make it easier in the long run.

--- PrelimAnal.R
+++ PrelimAnal.R
@@ -9,36 +9,70 @@ library(reshape2)
 # Import Data
 NokkenPooleHouseScores <- read.csv("C:/Users/Olson/Dropbox/ideas/Tea Party Does Not Exist/NokkenPooleHouse.csv")
 
+## These change the data for my purposes. One thing to note is that they drop all third
+## party members. This is something that needs to be considered eventually. Also need to drop
+## Presidents.
 # Only Use Data for 110th onward
-dav110 <- NokkenPooleHouseScores[ which(NokkenPooleHouseScores$Congress >= 110), ]
-# Only Post War
-dav80 <- NokkenPooleHouseScores[ which(NokkenPooleHouseScores$Congress >= 80), ]
+dav110 <- NokkenPooleHouseScores[ which(NokkenPooleHouseScores$Congress >= 110 & NokkenPooleHouseScores$Party <=200), ]
+# Only Post World War Two
+dav80 <- NokkenPooleHouseScores[ which(NokkenPooleHouseScores$Congress >= 80 & NokkenPooleHouseScores$Party <=200), ]
 # Only 20th Century
-dav56 <- NokkenPooleHouseScores[ which(NokkenPooleHouseScores$Congress >= 56), ]
+dav56 <- NokkenPooleHouseScores[ which(NokkenPooleHouseScores$Congress >= 56 & NokkenPooleHouseScores$Party <=200), ]
+
+### This section recodes the party into a 1/0 variable where Republican is 1. This section also
+### assigns a factor label to party which makes it easier to handle in subsiquent graphs / models
+### and also obviously adds party labels to graphs.
+## 110 onward
+dav110$Party <- ifelse(dav110$Party > 100, 
+                        c("1"), c("0")) 
+# Assign Party Labels
+dav110$Party <- factor(dav110$Party,
+                    levels = c(0,1),
+                    labels = c("Democrat", "Republican"))
+## Post WW2
+dav80$Party <- ifelse(dav80$Party > 100, 
+                        c("1"), c("0")) 
+# Assign Party Labels
+dav80$Party <- factor(dav80$Party,
+                       levels = c(0,1),
+                       labels = c("Democrat", "Republican"))
+
+## 20th Century
+dav56$Party <- ifelse(dav56$Party > 100, 
+                        c("1"), c("0")) 
+# Assign Party Labels
+dav56$Party <- factor(dav56$Party,
+                       levels = c(0,1),
+                       labels = c("Democrat", "Republican"))
 
 
 ### Part One
 ### Prelim Analysis
 ### Dec 15 update: Turns out my data was broken so a bunch of data was not being plotted correctly.
 ### Some of the first dimension nominate had letters first so it was messing stuff up.
-
-
-# Rough Graph It.
+## Rough Graph It.
 # This doesn't work and is way too messy. Dropping data
 p1gv1all <- ggplot(NokkenPooleHouseScores, aes(Congress, FirstDimension))
 p1gv1all + geom_point()
-p1gv1all + geom_point(aes(colour = factor(Party)))
+p1gv1all + geom_point(aes(colour = Party))
 dev.copy(png,'p1gv1all.png')
 dev.off()
 
 # For fun, look at second dimension which is racial data.
 p1gv3all <- ggplot(NokkenPooleHouseScores, aes(Congress, SecondDimension))
 p1gv3all + geom_point()
-p1gv3all + geom_point(aes(colour = factor(Party)))
+p1gv3all + geom_point(aes(colour = Party))
 
-# Second Graph Attempt
+# Second Graph Attempt | This looks dumb
 p1gv2 <- ggplot(dav110, aes(Congress, FirstDimension))
-p1gv2 + geom_point(aes(colour = factor(Party)))
+p1gv2 + geom_point(aes(colour = Party))
+
+# Here is a pretty good one examining the shift of Republicans in post war America
+p1gv4ww2 <- ggplot(dav80, aes(Congress, FirstDimension))
+p1gv4ww2 + geom_point()
+p1gv4ww2 + geom_point(aes(colour = Party))
+dev.copy(png,'p1gv4all.png')
+dev.off()
 
 ### Part Two
 ### For something totally different now, need to examine average ideoology
@@ -105,4 +139,6 @@ p2g2 <- ggplot(p2daAgg, aes((x=p2daAgg$Congress, y=p2daAgg$FirstDimension, color
 ## with just normal Republicans.
 
 MODEL HERE
+
+
                
